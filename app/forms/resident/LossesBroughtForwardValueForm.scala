665eae4db78f49e8bf95535ae1f7ac064aa29bf2
James Forster <james.forster@digital.hmrc.gov.uk>
2016-07-07 16:43:08 +0100

    Added generic error messages to Losses Brought Forward Value and Reliefs Value.

--- app/forms/resident/LossesBroughtForwardValueForm.scala
+++ app/forms/resident/LossesBroughtForwardValueForm.scala
@@ -16,24 +16,26 @@
 
 package forms.resident
 
+import common.Constants
 import models.resident.LossesBroughtForwardValueModel
 import play.api.data.Forms._
 import play.api.data._
 import common.Transformers._
 import common.Validation._
 import play.api.i18n.Messages
+import uk.gov.hmrc.play.views.helpers.MoneyPounds
 
 object LossesBroughtForwardValueForm {
 
   val lossesBroughtForwardValueForm = Form(
     mapping(
       "amount" -> text
-        .verifying(Messages("calc.base.undefinedMessage"), mandatoryCheck)
-        .verifying(Messages("calc.base.undefinedMessage"), bigDecimalCheck)
+        .verifying(Messages("calc.common.error.mandatoryAmount"), mandatoryCheck)
+        .verifying(Messages("calc.common.error.invalidAmount"), bigDecimalCheck)
         .transform[BigDecimal](stringToBigDecimal, bigDecimalToString)
-        .verifying(Messages("calc.base.undefinedMessage"), maxCheck)
-        .verifying(Messages("calc.base.undefinedMessage"), minCheck)
-        .verifying(Messages("calc.base.undefinedMessage"), decimalPlacesCheck)
+        .verifying(Messages("calc.common.error.maxAmountExceeded", MoneyPounds(Constants.maxNumeric, 0).quantity), maxCheck)
+        .verifying(Messages("calc.common.error.minimumAmount"), minCheck)
+        .verifying(Messages("calc.common.error.invalidAmount"), decimalPlacesCheck)
     )(LossesBroughtForwardValueModel.apply)(LossesBroughtForwardValueModel.unapply)
   )
 
