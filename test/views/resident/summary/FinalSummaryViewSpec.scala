68bde721ae11c28e60b1a3bf3b15d11fe528c084
david.woodward <david.woodward@digital.hmrc.gov.uk>
2016-07-08 11:56:10 +0100

    Added the missing tests.

--- test/views/resident/summary/FinalSummaryViewSpec.scala
+++ test/views/resident/summary/FinalSummaryViewSpec.scala
@@ -178,8 +178,14 @@ class FinalSummaryViewSpec extends UnitSpec with WithFakeApplication with FakeRe
         "Should have the question text 'Tax Rate'" in {
           doc.select("#gainAndRate-question").text shouldBe messages.taxRate
         }
-      }
 
+        "Should have the value £30,000" in {
+          doc.select("#firstBand").text should include("£30,000")
+        }
+        "Should have the tax rate 18%" in {
+          doc.select("#firstBand").text should include("18%")
+        }
+      }
     }
 
     s"have a section for Your answers" which {
@@ -370,7 +376,7 @@ class FinalSummaryViewSpec extends UnitSpec with WithFakeApplication with FakeRe
     }
   }
 
-  "Final Summary view with a calculation that returns tax on both side of the rate boundary" should {
+  "Final Summary view with a calculation has some previous taxable gains" should {
     lazy val gainAnswers = YourAnswersSummaryModel(Dates.constructDate(10, 10, 2016),
       BigDecimal(200000),
       BigDecimal(10000),
@@ -442,13 +448,27 @@ class FinalSummaryViewSpec extends UnitSpec with WithFakeApplication with FakeRe
       }
     }
 
+    "has a numeric output row and a tax rate" which {
+
+      "Should have the question text 'Tax Rate'" in {
+        doc.select("#gainAndRate-question").text shouldBe messages.taxRate
+      }
+
+      "Should have the value £30,000" in {
+        doc.select("#firstBand").text should include("£30,000")
+      }
+      "Should have the tax rate 18%" in {
+        doc.select("#firstBand").text should include("18%")
+      }
+    }
+
     "has an option output row for previous taxable gains" which {
 
       s"should have the question text '${commonMessages.previousTaxableGains.title}'" in {
         doc.select("#previousTaxableGains-question").text shouldBe commonMessages.previousTaxableGains.title
       }
 
-      "should have the value '£0'" in {
+      "should have the value '£1,000'" in {
         doc.select("#previousTaxableGains-amount span").text shouldBe "£1,000"
       }
 
@@ -487,4 +507,85 @@ class FinalSummaryViewSpec extends UnitSpec with WithFakeApplication with FakeRe
     }
   }
 
+  "Final Summary view with a calculation that returns tax on both side of the rate boundary" should {
+    lazy val gainAnswers = YourAnswersSummaryModel(Dates.constructDate(10, 10, 2016),
+      BigDecimal(200000),
+      BigDecimal(10000),
+      BigDecimal(100000),
+      BigDecimal(10000),
+      BigDecimal(30000))
+    lazy val deductionAnswers = ChargeableGainAnswers(Some(ReliefsModel(false)),
+      None,
+      Some(OtherPropertiesModel(true)),
+      Some(AllowableLossesModel(false)),
+      None,
+      Some(LossesBroughtForwardModel(false)),
+      None,
+      Some(AnnualExemptAmountModel(0)))
+
+    lazy val incomeAnswers = IncomeAnswersModel(Some(PreviousTaxableGainsModel(1000)), Some(CurrentIncomeModel(0)), Some(PersonalAllowanceModel(0)))
+
+    lazy val results = TotalGainAndTaxOwedModel(
+      50000,
+      20000,
+      0,
+      30000,
+      3600,
+      30000,
+      18,
+      Some(10000),
+      Some(28)
+    )
+
+    lazy val backLink = "/calculate-your-capital-gains/resident/personal-allowance"
+
+    lazy val view = views.finalSummary(gainAnswers, deductionAnswers, incomeAnswers, results, backLink)(fakeRequestWithSession)
+    lazy val doc = Jsoup.parse(view.body)
+
+    "have a charset of UTF-8" in {
+      doc.charset().toString shouldBe "UTF-8"
+    }
+
+    s"have a title ${messages.title}" in {
+      doc.title() shouldBe messages.title
+    }
+
+    s"have a back button" which {
+
+      lazy val backLink = doc.getElementById("back-link")
+
+      "has the id 'back-link'" in {
+        backLink.attr("id") shouldBe "back-link"
+      }
+
+      s"has the text '${commonMessages.calcBaseBack}'" in {
+        backLink.text shouldBe commonMessages.calcBaseBack
+      }
+
+      s"has a link to '${routes.IncomeController.personalAllowance().toString()}'" in {
+        backLink.attr("href") shouldBe routes.IncomeController.personalAllowance().toString
+      }
+    }
+
+    "has a numeric output row and a tax rate" which {
+
+      "Should have the question text 'Tax Rate'" in {
+        doc.select("#gainAndRate-question").text shouldBe messages.taxRate
+      }
+
+      "Should have the value £30,000 in the first band" in {
+        doc.select("#firstBand").text should include("£30,000")
+      }
+      "Should have the tax rate 18% for the first band" in {
+        doc.select("#firstBand").text should include("18%")
+      }
+
+      "Should have the value £10,000 in the second band" in {
+        doc.select("#secondBand").text should include("£10,000")
+      }
+      "Should have the tax rate 28% for the first band" in {
+        doc.select("#secondBand").text should include("28%")
+      }
+    }
+  }
 }
