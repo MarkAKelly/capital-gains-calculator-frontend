e266d130288fa79e98bfe40d23fc0ab5ff2ec344
matthew.page <matthew.page@digital.hmrc.gov.uk>
2016-08-11 12:14:21 +0100

    Removed console logging from GA Javascript

--- public/javascripts/cgt.js
+++ public/javascripts/cgt.js
@@ -47,7 +47,6 @@ $(document).ready($(function () {
     var metrics = $(this).attr('data-metrics');
     var parts = metrics.split(':');
     ga('send', 'event', parts[0], parts[1], parts[2]);
-    console.log("sending: " + parts[0] + " - " + parts[1] + " - " + parts[2])
   });
 
 }));
