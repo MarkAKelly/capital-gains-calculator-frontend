96270a26b69b7f881d38fdd4ecd4b2aea700f198
James Forster <james.forster@digital.hmrc.gov.uk>
2016-09-06 16:56:05 +0100

    Added Lettings Relief question to Deductions Summary.

--- test/views/resident/properties/summary/PropertiesDeductionsSummaryViewSpec.scala
+++ test/views/resident/properties/summary/PropertiesDeductionsSummaryViewSpec.scala
@@ -816,9 +816,9 @@ class PropertiesDeductionsSummaryViewSpec extends UnitSpec with WithFakeApplicat
       Some(LossesBroughtForwardModel(true)),
       Some(LossesBroughtForwardValueModel(10000)),
       Some(AnnualExemptAmountModel(1000)),
-      Some(PropertyLivedInModel(false)),
-      None,
-      None)
+      Some(PropertyLivedInModel(true)),
+      Some(PrivateResidenceReliefModel(true)),
+      Some(LettingsReliefModel(true)))
     lazy val results = ChargeableGainResultModel(BigDecimal(50000),
       BigDecimal(-11000),
       BigDecimal(0),
@@ -869,6 +869,52 @@ class PropertiesDeductionsSummaryViewSpec extends UnitSpec with WithFakeApplicat
         doc.select("#allowableLossRemaining-amount div span").text() should
           include(s"${messages.remainingLossHelp} ${messages.remainingLossLink} " +
             s"${commonMessages.calcBaseExternalLink} ${messages.remainingAllowableLossHelp}")
+      }
+    }
+
+    "has an option output row for eligible for private residence relief in" which {
+
+      s"should have the question text '${commonMessages.privateResidenceRelief.title}'" in {
+        doc.select("#privateResidenceRelief-question").text shouldBe commonMessages.privateResidenceRelief.title
+      }
+
+      "should have the value 'Yes'" in {
+        doc.select("#privateResidenceRelief-option span.bold-medium").text shouldBe "Yes"
+      }
+
+      s"should have a change link to ${routes.DeductionsController.privateResidenceRelief().url}" in {
+        doc.select("#privateResidenceRelief-option a").attr("href") shouldBe routes.DeductionsController.privateResidenceRelief().url
+      }
+
+      "has the question as part of the link" in {
+        doc.select("#privateResidenceRelief-option a").text shouldBe s"${commonMessages.calcBaseChange} ${commonMessages.privateResidenceRelief.title}"
+      }
+
+      "has the question component of the link as visuallyhidden" in {
+        doc.select("#privateResidenceRelief-option a span.visuallyhidden").text shouldBe commonMessages.privateResidenceRelief.title
+      }
+    }
+
+    "has an option output row for eligible for lettings relief in" which {
+
+      s"should have the question text '${commonMessages.lettingsRelief.title}'" in {
+        doc.select("#lettingsRelief-question").text shouldBe commonMessages.lettingsRelief.title
+      }
+
+      "should have the value 'Yes'" in {
+        doc.select("#lettingsRelief-option span.bold-medium").text shouldBe "Yes"
+      }
+
+      s"should have a change link to ${routes.DeductionsController.lettingsRelief().url}" in {
+        doc.select("#lettingsRelief-option a").attr("href") shouldBe routes.DeductionsController.lettingsRelief().url
+      }
+
+      "has the question as part of the link" in {
+        doc.select("#lettingsRelief-option a").text shouldBe s"${commonMessages.calcBaseChange} ${commonMessages.lettingsRelief.title}"
+      }
+
+      "has the question component of the link as visuallyhidden" in {
+        doc.select("#lettingsRelief-option a span.visuallyhidden").text shouldBe commonMessages.lettingsRelief.title
       }
     }
 
