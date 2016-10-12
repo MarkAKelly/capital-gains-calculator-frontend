7423bfb270d7db2a3c79a84bcbdd4b72b0a736d8
Thomas Dudley <thomas.dudley@digital.hmrc.gov.uk>
2016-10-12 15:34:05 +0100

    Added controller and updated messages

---
+++ app/controllers/nonresident/ReportController.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.nonresident
+
+import java.time.LocalDate
+
+import common.Dates._
+import common.KeystoreKeys
+import connectors.CalculatorConnector
+import controllers.predicates.ValidActiveSession
+import it.innove.play.pdf.PdfGenerator
+import models.nonresident.{CalculationResultModel, DisposalDateModel, SummaryModel}
+import models.resident.TaxYearModel
+import play.api.i18n.Messages
+import uk.gov.hmrc.play.frontend.controller.FrontendController
+import play.api.mvc.{Action, RequestHeader}
+import uk.gov.hmrc.play.http.HeaderCarrier
+import views.html.calculation
+import views.html.calculation.nonresident.{summaryReport => summaryView}
+
+import scala.concurrent.Future
+
+
+object ReportController extends ReportController {
+  val calcConnector = CalculatorConnector
+}
+
+trait ReportController extends FrontendController with ValidActiveSession {
+
+  val calcConnector: CalculatorConnector
+
+  def host(implicit request: RequestHeader): String ={
+    s"http://${request.host}/"
+  }
+
+  def getTaxYear(disposalDate: DisposalDateModel)(implicit hc: HeaderCarrier): Future[Option[TaxYearModel]] =
+    calcConnector.getTaxYear(disposalDate.year.toString)
+
+  def resultModel(summaryModel: SummaryModel)(implicit hc: HeaderCarrier): Future[Option[CalculationResultModel]] = {
+    summaryModel.calculationElectionModel.calculationType match {
+      case "flat" =>
+        calcConnector.calculateFlat(summaryModel)
+      case "time" =>
+        calcConnector.calculateTA(summaryModel)
+      case "rebased" =>
+        calcConnector.calculateRebased(summaryModel)
+    }
+  }
+
+  val summaryReport = ValidateSession.async { implicit request =>
+    for {
+      summary <- calcConnector.createSummary(hc)
+      result <- resultModel(summary)(hc)
+      taxYear <- getTaxYear(summary.disposalDateModel)
+
+    } yield {
+      PdfGenerator.ok(summaryView(summary, result.get, taxYear.get), host).toScala
+        .withHeaders("Content-Disposition" ->s"""attachment; filename="${Messages("calc.summary.title")}.pdf""")
+    }
+  }
+}
---
+++ test/controllers/CalculationControllerTests/ReportSpec.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.CalculationControllerTests
+
+import connectors.CalculatorConnector
+import controllers.helpers.FakeRequestHelper
+import controllers.nonresident.ReportController
+import org.scalatest.mock.MockitoSugar
+import play.api.mvc.RequestHeader
+import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
+
+class ReportSpec extends UnitSpec with WithFakeApplication with FakeRequestHelper with MockitoSugar {
+
+  def setupTarget
+  (
+
+  ): ReportController = {
+
+    lazy val mockCalculatorConnector = mock[CalculatorConnector]
+
+    new ReportController {
+      override val calcConnector: CalculatorConnector = mockCalculatorConnector
+      override def host(implicit request: RequestHeader): String = "http://localhost:9977"
+    }
+  }
+
+}
