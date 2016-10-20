5396975f05dfb6e6a0adf5742e420a5aa056cb77
Thomas Dudley <thomas.dudley@digital.hmrc.gov.uk>
2016-10-20 13:41:57 +0100

    Commiting to do pull requesut

---
+++ test/constructors/nonresident/PersonalDetailsConstructorSpec.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package constructors.nonresident
+
+import common.nonresident.CustomerTypeKeys
+import models.nonresident._
+import uk.gov.hmrc.play.test.UnitSpec
+
+class PersonalDetailsConstructorSpec extends UnitSpec {
+
+//  val summaryModel = SummaryModel(
+//    CustomerTypeModel(CustomerTypeKeys.individual),
+//    DisabledTrusteeModel("Yes"),
+//    CurrentIncomeModel(30000.0),
+//    PersonalAllowanceModel(11000.0),
+//    OtherPropertiesModel("Yes", 250000.0),
+//    AnnualExemptAmountModel("Yes", 10000.0),
+//    AcquisitionDateModel("Yes", Some(4), Some(9), Some(2016)),
+//    AcquisitionValueModel()
+//  )
+
+}
