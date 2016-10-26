f8b50657414a160dbffe59c737325674b86ff00e
Emma-Louise Upton <emma-louise.upton@hmrcaspire.com>
2016-10-26 14:05:03 +0100

    Added further tests to test for No values

--- test/constructors/nonresident/PurchaseDetailsConstructorSpec.scala
+++ test/constructors/nonresident/PurchaseDetailsConstructorSpec.scala
@@ -54,6 +54,30 @@ object PurchaseDetailsConstructorSpec extends UnitSpec with WithFakeApplication 
     Some(PrivateResidenceReliefModel("Yes", Some(2500.0), Some(0.0)))
   )
 
+  val summaryNoOptionsModel = SummaryModel(
+    CustomerTypeModel(CustomerTypeKeys.individual),
+    None,
+    None,
+    None,
+    OtherPropertiesModel("Yes", None),
+    None,
+    AcquisitionDateModel("No", None, None, None),
+    AcquisitionValueModel(300000.0),
+    None,
+    None,
+    ImprovementsModel("No", None),
+    DisposalDateModel(5, 9, 2016),
+    DisposalValueModel(5000),
+    AcquisitionCostsModel(250000.0),
+    DisposalCostsModel(5000.0),
+    AllowableLossesModel("No", None),
+    CalculationElectionModel("flat"),
+    OtherReliefsModel(None, None),
+    OtherReliefsModel(None, None),
+    OtherReliefsModel(None, None),
+    None
+  )
+
   "Calling the PurchaseDetailsConstructor" when {
 
     "using the summaryWithAllOptionsValuesModel" should {
@@ -143,7 +167,114 @@ object PurchaseDetailsConstructorSpec extends UnitSpec with WithFakeApplication 
         obtainedKey shouldBe KeystoreKeys.acquisitionDate
       }
 
-      //test URL
+      ".getAcquisitionDateAnswer will return a url of " +
+        s"${constructors.nonresident.PurchaseDetailsConstructor
+          .getAcquisitionDateAnswer(summaryWithAllOptionValuesModel).toString}" in {
+        val obtainedUrl = constructors.nonresident.PurchaseDetailsConstructor
+          .getAcquisitionDateAnswer(summaryWithAllOptionValuesModel).toString
+        obtainedUrl shouldBe constructors.nonresident.PurchaseDetailsConstructor
+          .getAcquisitionDateAnswer(summaryWithAllOptionValuesModel).toString
+      }
+
+      ".getAcquisitionValueAnswer will return a value of 30000.0" in {
+        val obtainedValue = PurchaseDetailsConstructor
+          .getAcquisitionValueAnswer(summaryWithAllOptionValuesModel).get.data
+        obtainedValue shouldBe 30000.0
+      }
+
+      ".getAcquisitionValue will return an id of " + s"${KeystoreKeys.acquisitionValue}" in {
+        val obtainedKey = PurchaseDetailsConstructor
+          .getAcquisitionValueAnswer(summaryWithAllOptionValuesModel).get.id
+        obtainedKey shouldBe KeystoreKeys.acquisitionValue
+      }
+
+      ".getAcquisitionValueAnswer will return a url of " +
+        s"${constructors.nonresident.PurchaseDetailsConstructor
+          .getAcquisitionValueAnswer(summaryWithAllOptionValuesModel).toString}" in {
+        val obtainedUrl = constructors.nonresident.PurchaseDetailsConstructor
+          .getAcquisitionValueAnswer(summaryWithAllOptionValuesModel).toString
+        obtainedUrl shouldBe constructors.nonresident.PurchaseDetailsConstructor
+          .getAcquisitionValueAnswer(summaryWithAllOptionValuesModel).toString
+      }
+
+
+      ".getAcquisitionCostsAnswer will return a value of 250000.0" in {
+        val obtainedValue = PurchaseDetailsConstructor
+          .getAcquisitionCostsAnswer(summaryWithAllOptionValuesModel).get.data
+        obtainedValue shouldBe 250000.0
+      }
+
+      ".getAcquisitionCostsAnswer will return an id of " + s"${KeystoreKeys.acquisitionCosts}" in {
+        val obtainedKey = PurchaseDetailsConstructor
+          .getAcquisitionCostsAnswer(summaryWithAllOptionValuesModel).get.id
+        obtainedKey shouldBe KeystoreKeys.acquisitionCosts
+      }
+
+      ".getAcquisitionCostsAnswer will return a url of " +
+        s"${constructors.nonresident.PurchaseDetailsConstructor
+          .getAcquisitionValueAnswer(summaryWithAllOptionValuesModel).toString}" in {
+        val obtainedUrl = constructors.nonresident.PurchaseDetailsConstructor
+          .getAcquisitionCostsAnswer(summaryWithAllOptionValuesModel).toString
+        obtainedUrl shouldBe constructors.nonresident.PurchaseDetailsConstructor
+          .getAcquisitionCostsAnswer(summaryWithAllOptionValuesModel).toString
+      }
+
+      ".getRebasedValue will return a value of 350000.0" in {
+        val obtainedValue = PurchaseDetailsConstructor
+          .getRebasedValueAnswer(summaryWithAllOptionValuesModel).get.data
+        obtainedValue shouldBe 350000.0
+      }
+
+      ".getRebasedValue will return an id of " + s"${KeystoreKeys.rebasedValue}" in {
+        val obtainedKey = PurchaseDetailsConstructor
+          .getRebasedValueAnswer(summaryWithAllOptionValuesModel).get.id
+        obtainedKey shouldBe KeystoreKeys.rebasedValue
+      }
+
+      ".getRebasedValueAnswer will return a url of " +
+        s"${constructors.nonresident.PurchaseDetailsConstructor
+          .getAcquisitionValueAnswer(summaryWithAllOptionValuesModel).toString}" in {
+        val obtainedUrl = constructors.nonresident.PurchaseDetailsConstructor
+          .getRebasedValueAnswer(summaryWithAllOptionValuesModel).toString
+        obtainedUrl shouldBe constructors.nonresident.PurchaseDetailsConstructor
+          .getRebasedValueAnswer(summaryWithAllOptionValuesModel).toString
+      }
+
+      ".getRebasedCostsAnswer will return a value of 250000.0" in {
+        val obtainedValue = PurchaseDetailsConstructor
+          .getRebasedCostsAnswer(summaryWithAllOptionValuesModel).get.data
+        obtainedValue shouldBe 250000.0
+      }
+
+      ".getRebasedCostsAnswer will return an id of " + s"${KeystoreKeys.rebasedCosts}" in {
+        val obtainedKey = PurchaseDetailsConstructor
+          .getRebasedValueAnswer(summaryWithAllOptionValuesModel).get.id
+        obtainedKey shouldBe KeystoreKeys.rebasedCosts
+      }
+
+      ".getRebasedCostsAnswer will return a url of " +
+        s"${constructors.nonresident.PurchaseDetailsConstructor
+          .getRebasedCostsAnswer(summaryWithAllOptionValuesModel).toString}" in {
+        val obtainedUrl = constructors.nonresident.PurchaseDetailsConstructor
+          .getRebasedCostsAnswer(summaryWithAllOptionValuesModel).toString
+        obtainedUrl shouldBe constructors.nonresident.PurchaseDetailsConstructor
+          .getRebasedCostsAnswer(summaryWithAllOptionValuesModel).toString
+      }
     }
+
+    "using the summaryNoOptionsModel" should {
+      ".getRebasedValueAnswer will return data of None" in {
+        val obtainedData = PurchaseDetailsConstructor
+          .getRebasedValueAnswer(summaryNoOptionsModel)
+        obtainedData shouldBe None
+      }
+
+      ".getRebasedCostsAnswer will return data of None" in {
+        val obtainedData = PurchaseDetailsConstructor
+          .getRebasedCostsAnswer(summaryNoOptionsModel)
+        obtainedData shouldBe None
+      }
+    }
+
   }
 }
