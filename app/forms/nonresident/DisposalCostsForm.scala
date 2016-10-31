4d4f384be9a41a25fc130507b5519a1fd938313f
Thomas Dudley <thomas.dudley@digital.hmrc.gov.uk>
2016-10-31 14:30:09 +0000

    Added DisposalCostsFormSpec, updated model

--- app/forms/nonresident/DisposalCostsForm.scala
+++ app/forms/nonresident/DisposalCostsForm.scala
@@ -17,6 +17,7 @@
 package forms.nonresident
 
 import common.Constants
+import common.Transformers._
 import common.Validation._
 import models.nonresident.DisposalCostsModel
 import play.api.data.Forms._
@@ -25,11 +26,14 @@ import play.api.i18n.Messages
 import uk.gov.hmrc.play.views.helpers.MoneyPounds
 
 object DisposalCostsForm {
-  val disposalCostsForm = Form(
+  def disposalCostsForm(disposalCosts: BigDecimal = BigDecimal(0)): Form[DisposalCostsModel] = Form(
     mapping(
-      "disposalCosts" -> bigDecimal
-        .verifying(Messages("calc.disposalCosts.errorNegativeNumber"), isPositive)
-        .verifying(Messages("calc.disposalCosts.errorDecimalPlaces"), decimalPlacesCheck)
+      "disposalCosts" -> text
+        .verifying(Messages("error.real"), mandatoryCheck)
+        .verifying(Messages("error.real"), bigDecimalCheck)
+        .transform(stringToBigDecimal, bigDecimalToString)
+        .verifying(Messages("calc.disposalCosts.errorNegativeNumber"), costs => isPositive(costs))
+        .verifying(Messages("calc.disposalCosts.errorDecimalPlaces"), costs => decimalPlacesCheck(costs))
         .verifying(Messages("calc.common.error.maxNumericExceeded") + MoneyPounds(Constants.maxNumeric, 0).quantity + " " +
           Messages("calc.common.error.maxNumericExceeded.OrLess"), maxCheck)
     )(DisposalCostsModel.apply)(DisposalCostsModel.unapply)
