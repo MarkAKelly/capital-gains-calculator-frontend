078bb7b756a289c283736e098c1c2fc4a0627dd4
david.woodward <david.woodward@digital.hmrc.gov.uk>
2016-11-02 11:49:05 +0000

    Refactored the action spec.

--- test/controllers/CalculationControllerTests/PersonalAllowanceSpec.scala
+++ test/controllers/CalculationControllerTests/PersonalAllowanceSpec.scala
@@ -16,17 +16,13 @@
 
 package controllers.CalculationControllerTests
 
-import assets.MessageLookup.{NonResident => commonMessages}
 import assets.MessageLookup.NonResident.{PersonalAllowance => messages}
 import connectors.CalculatorConnector
-import play.api.libs.json.Json
-import uk.gov.hmrc.http.cache.client.CacheMap
+import controllers.helpers.FakeRequestHelper
 import org.mockito.Matchers
 import org.mockito.Mockito._
-import play.api.mvc.AnyContentAsFormUrlEncoded
-import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import uk.gov.hmrc.play.http.{HeaderCarrier, SessionKeys}
+import uk.gov.hmrc.play.http.HeaderCarrier
 import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
 import org.jsoup._
 import org.scalatest.mock.MockitoSugar
@@ -34,13 +30,12 @@ import org.scalatest.mock.MockitoSugar
 import scala.concurrent.Future
 import controllers.nonresident.{PersonalAllowanceController, routes}
 import models.nonresident.PersonalAllowanceModel
-import play.api.mvc.Result
 
-class PersonalAllowanceSpec extends UnitSpec with WithFakeApplication with MockitoSugar {
+class PersonalAllowanceSpec extends UnitSpec with WithFakeApplication with MockitoSugar with FakeRequestHelper {
 
   implicit val hc = new HeaderCarrier()
 
-  def setupTarget(getData: Option[PersonalAllowanceModel], postData: Option[PersonalAllowanceModel]): PersonalAllowanceController = {
+  def setupTarget(getData: Option[PersonalAllowanceModel]): PersonalAllowanceController = {
 
     val mockCalcConnector = mock[CalculatorConnector]
 
@@ -50,25 +45,46 @@ class PersonalAllowanceSpec extends UnitSpec with WithFakeApplication with Mocki
     when(mockCalcConnector.getPA(Matchers.anyInt(), Matchers.anyBoolean())(Matchers.any()))
       .thenReturn(Some(BigDecimal(11000)))
 
-    lazy val data = CacheMap("form-id", Map("data" -> Json.toJson(postData.getOrElse(PersonalAllowanceModel(0)))))
-    when(mockCalcConnector.saveFormData[PersonalAllowanceModel](Matchers.anyString(), Matchers.any())(Matchers.any(), Matchers.any()))
-      .thenReturn(Future.successful(data))
-
     new PersonalAllowanceController {
       override val calcConnector: CalculatorConnector = mockCalcConnector
-     
     }
   }
 
   // GET Tests
-  "Calling the PersonalAllowanceController.customerType" when {
+  "Calling the PersonalAllowanceController.personalAllowance" when {
 
-    lazy val fakeRequest = FakeRequest("GET", "/calculate-your-capital-gains/non-resident/personal-allowance").withSession(SessionKeys.sessionId -> "12345")
-
-    "not supplied with a pre-existing stored model" should {
-
-      val target = setupTarget(None, None)
+    "called with no session" should {
+      lazy val target = setupTarget(None)
       lazy val result = target.personalAllowance(fakeRequest)
+
+      "return a 303" in {
+        status(result) shouldBe 303
+      }
+
+      s"redirect to /calculate-your-capital-gains/session-timeout" in {
+        redirectLocation(result).get should include("/calculate-your-capital-gains/session-timeout")
+      }
+    }
+
+    "not supplied with a pre-existing stored model with a session" should {
+
+      lazy val target = setupTarget(None)
+      lazy val result = target.personalAllowance(fakeRequestWithSession)
+      lazy val document = Jsoup.parse(bodyOf(result))
+
+      "return a 200" in {
+        status(result) shouldBe 200
+      }
+
+      s"have the title ${messages.question}" in {
+        document.title shouldEqual messages.question
+      }
+    }
+
+    "supplied with a pre-existing model with a session" should {
+
+      lazy val target = setupTarget(Some(PersonalAllowanceModel(10000)))
+      lazy val result = target.personalAllowance(fakeRequestWithSession)
       lazy val document = Jsoup.parse(bodyOf(result))
 
       "return a 200" in {
@@ -84,24 +100,11 @@ class PersonalAllowanceSpec extends UnitSpec with WithFakeApplication with Mocki
   // POST Tests
   "In PersonalAllowanceController calling the .submitPersonalAllowance action" when {
 
-    def buildRequest(body: (String, String)*): FakeRequest[AnyContentAsFormUrlEncoded] = FakeRequest("POST",
-      "/calculate-your-capital-gains/non-resident/personal-allowance")
-      .withSession(SessionKeys.sessionId -> "12345")
-      .withFormUrlEncodedBody(body: _*)
-
-    def executeTargetWithMockData(data: String): Future[Result] = {
-      lazy val fakeRequest = buildRequest(("personalAllowance", data))
-      val mockData = data match {
-        case "" => None
-        case _ => Some(PersonalAllowanceModel(BigDecimal(data)))
-      }
-      val target = setupTarget(None, mockData)
-      target.submitPersonalAllowance(fakeRequest)
-    }
-
     "submitting a valid form with '1000'" should {
 
-      lazy val result = executeTargetWithMockData("1000")
+      lazy val target = setupTarget(None)
+      lazy val request = fakeRequestToPOSTWithSession(("personalAllowance", "10000"))
+      lazy val result = target.submitPersonalAllowance(request)
 
       "return a 303" in {
         status(result) shouldBe 303
@@ -110,67 +113,22 @@ class PersonalAllowanceSpec extends UnitSpec with WithFakeApplication with Mocki
       s"redirect to ${routes.OtherPropertiesController.otherProperties()}" in {
         redirectLocation(result) shouldBe Some(s"${routes.OtherPropertiesController.otherProperties()}")
       }
+    }
 
 
-      "submitting an invalid form with no value" should {
+    "submitting an invalid form with no value" should {
 
-        lazy val result = executeTargetWithMockData("")
+      lazy val target = setupTarget(None)
+      lazy val request = fakeRequestToPOSTWithSession(("personalAllowance", "-12139"))
+      lazy val result = target.submitPersonalAllowance(request)
+      lazy val document = Jsoup.parse(bodyOf(result))
 
-        "return a 400" in {
-          status(result) shouldBe 400
-        }
+      "return a 400" in {
+        status(result) shouldBe 400
       }
 
-      "submitting an invalid form with a negative value of -342" should {
-
-        lazy val result = executeTargetWithMockData("-342")
-
-        "return a 400" in {
-          status(result) shouldBe 400
-        }
-      }
-
-      "submitting an invalid form with value 1.111" should {
-
-        lazy val result = executeTargetWithMockData("1.111")
-        lazy val document = Jsoup.parse(bodyOf(result))
-
-        "return a 400" in {
-          status(result) shouldBe 400
-        }
-
-        s"fail with message ${messages.errorDecimalPlaces}" in {
-          document.getElementsByClass("error-notification").text should include(messages.errorDecimalPlaces)
-        }
-      }
-
-      "submitting a form which exceeds the maximum PA amount" should {
-
-        lazy val result = executeTargetWithMockData("11001")
-        lazy val document = Jsoup.parse(bodyOf(result))
-
-        "return a 400" in {
-          status(result) shouldBe 400
-        }
-
-        s"fail with message ${messages.errorMaxLimit}" in {
-          document.getElementsByClass("error-notification").text should include(messages.errorMaxLimit)
-        }
-      }
-
-      "submitting a form which exceeds the maximum PA amount and has fractional pounds" should {
-
-        lazy val result = executeTargetWithMockData("12100.01")
-        lazy val document = Jsoup.parse(bodyOf(result))
-
-        "return a 400" in {
-          status(result) shouldBe 400
-        }
-
-        s"fail with message ${messages.errorMaxLimit}" in {
-          document.getElementsByClass("error-notification").text should include(messages.errorMaxLimit)
-          document.getElementsByClass("error-notification").text should include(messages.errorDecimalPlaces)
-        }
+      s"return to the personal allowance page" in {
+        document.title shouldEqual messages.question
       }
     }
   }
