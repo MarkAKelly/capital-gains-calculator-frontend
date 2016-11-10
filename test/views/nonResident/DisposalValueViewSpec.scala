feb46007b545098fc2d1205349bc60a47f67d369
david.woodward <david.woodward@digital.hmrc.gov.uk>
2016-11-10 13:43:37 +0000

    Updated the heading size.

--- test/views/nonResident/DisposalValueViewSpec.scala
+++ test/views/nonResident/DisposalValueViewSpec.scala
@@ -51,8 +51,8 @@ class DisposalValueViewSpec extends UnitSpec with WithFakeApplication with Mocki
       "have a heading" which {
         lazy val heading = document.body().select("h1")
 
-        "has a class of heading-large" in {
-          heading.attr("class") shouldBe "heading-large"
+        "has a class of heading-xlarge" in {
+          heading.attr("class") shouldBe "heading-xlarge"
         }
 
         s"has the text '${messages.DisposalValue.question}'" in {
