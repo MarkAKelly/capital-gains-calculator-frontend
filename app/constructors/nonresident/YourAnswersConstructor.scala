b9a896dedcd5d61b4adcf87e5a0b320a40eb9bb7
James Forster <james.forster@digital.hmrc.gov.uk>
2016-11-11 16:34:17 +0000

    Added sale, purchase and property details to the aggregate constructor.

---
+++ app/constructors/nonresident/YourAnswersConstructor.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package constructors.nonresident
+
+import models.nonresident.{QuestionAnswerModel, SummaryModel}
+
+object YourAnswersConstructor {
+
+  //TODO update to use the new answers models
+  def fetchYourAnswers(summaryModel: SummaryModel): Seq[QuestionAnswerModel[Any]] = {
+    val salesDetailsRows = SalesDetailsConstructor.salesDetailsRows(summaryModel)
+    val purchaseDetailsRows = PurchaseDetailsConstructor.getPurchaseDetailsSection(summaryModel)
+    val propertyDetailsRows = PropertyDetailsConstructor.propertyDetailsRows(summaryModel)
+
+    for {
+      salesDetails <- salesDetailsRows
+      purchaseDetails <- purchaseDetailsRows
+      propertyDetails <- propertyDetailsRows
+      answerRows <- salesDetailsRows ++ purchaseDetailsRows ++ propertyDetailsRows
+    } yield answerRows
+  }
+}
---
+++ test/constructors/nonresident/YourAnswersConstructorSpec.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package constructors.nonresident
+
+import common.TestModels
+import uk.gov.hmrc.play.test.UnitSpec
+
+class YourAnswersConstructorSpec extends UnitSpec {
+
+  "Calling .fetchYourAnswers" when {
+
+    "only fetching total gain answers" should {
+      //TODO update to total gain when new model is available
+      val model = TestModels.sumModelFlat
+      val result = YourAnswersConstructor.fetchYourAnswers(model)
+
+      "contain the answers from sale details" in {
+        val salesDetails = SalesDetailsConstructor.salesDetailsRows(model)
+
+        result.containsSlice(salesDetails) shouldBe true
+      }
+
+      "contain the answers from purchase details" in {
+        val purchaseDetails = PurchaseDetailsConstructor.getPurchaseDetailsSection(model)
+
+        result.containsSlice(purchaseDetails) shouldBe true
+      }
+
+      "contain the answers from property details" in {
+        val propertyDetails = PropertyDetailsConstructor.propertyDetailsRows(model)
+
+        result.containsSlice(propertyDetails) shouldBe true
+      }
+    }
+  }
+
+}
