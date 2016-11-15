faba48da2095eefd68a2e4344ee2b016d0f87283
Emma-Louise Upton <emma-louise.upton@hmrcaspire.com>
2016-11-15 17:40:59 +0000

    Partial commit

--- test/views/helpers/nonresident/QuestionAnswerTableRowViewSpec.scala
+++ test/views/helpers/nonresident/QuestionAnswerTableRowViewSpec.scala
@@ -16,11 +16,26 @@
 
 package views.helpers.nonresident
 
+import models.nonresident.QuestionAnswerModel
+import org.jsoup.Jsoup
 import uk.gov.hmrc.play.test.UnitSpec
+import views.html.helpers.nonresident.questionAnswerTableRow
 
 /**
   * Created by emma on 15/11/16.
   */
 class QuestionAnswerTableRowViewSpec extends UnitSpec {
+  "Creating questionAnswerTableRow" when {
+    "passing in a String answer" should {
+      val model = QuestionAnswerModel[String]("id", "answer", "question", Some("change-link"))
+      val result = questionAnswerTableRow(model)
+      val doc = Jsoup.parse(result.body)
 
+      "have a table row with a table row for the question with ID id-question" which {
+        "has a question column with the question 'question'" in {
+          doc shouldBe ""
+        }
+      }
+    }
+  }
 }
