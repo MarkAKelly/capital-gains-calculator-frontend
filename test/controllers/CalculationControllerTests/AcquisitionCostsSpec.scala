462583be768bbd9645adc6fd9274829881f506be
James Forster <james.forster@digital.hmrc.gov.uk>
2016-11-21 10:04:14 +0000

    Added dynamic back-link to Acquisition Costs.

--- test/controllers/CalculationControllerTests/AcquisitionCostsSpec.scala
+++ test/controllers/CalculationControllerTests/AcquisitionCostsSpec.scala
@@ -16,10 +16,14 @@
 
 package controllers.CalculationControllerTests
 
+import assets.MessageLookup.NonResident.{AcquisitionCosts => messages}
+import common.KeystoreKeys
 import connectors.CalculatorConnector
 import constructors.nonresident.CalculationElectionConstructor
+import controllers.helpers.FakeRequestHelper
 import controllers.nonresident.AcquisitionCostsController
-import models.nonresident.AcquisitionCostsModel
+import models.nonresident.{AcquisitionCostsModel, AcquisitionDateModel, BoughtForLessModel}
+import models.resident.properties.HowBecameOwnerModel
 import org.jsoup.Jsoup
 import org.mockito.Matchers
 import org.mockito.Mockito._
@@ -27,21 +31,32 @@ import org.scalatest.mock.MockitoSugar
 import play.api.test.Helpers._
 import uk.gov.hmrc.play.http.HeaderCarrier
 import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
-import assets.MessageLookup.NonResident.{AcquisitionCosts => messages}
-import controllers.helpers.FakeRequestHelper
 
 import scala.concurrent.Future
 
 class AcquisitionCostsSpec extends UnitSpec with WithFakeApplication with MockitoSugar with FakeRequestHelper {
 
   implicit val hc = new HeaderCarrier()
-  def setupTarget(getData: Option[AcquisitionCostsModel]): AcquisitionCostsController = {
+
+  def setupTarget(getData: Option[AcquisitionCostsModel],
+                  acquisitionDateData: Option[AcquisitionDateModel] = None,
+                  howBecameOwnerData: Option[HowBecameOwnerModel] = None,
+                  boughtForLessData: Option[BoughtForLessModel] = None): AcquisitionCostsController = {
 
     val mockCalcConnector = mock[CalculatorConnector]
     val mockCalcElectionConstructor = mock[CalculationElectionConstructor]
 
-    when(mockCalcConnector.fetchAndGetFormData[AcquisitionCostsModel](Matchers.anyString())(Matchers.any(), Matchers.any()))
+    when(mockCalcConnector.fetchAndGetFormData[AcquisitionCostsModel](Matchers.eq(KeystoreKeys.acquisitionCosts))(Matchers.any(), Matchers.any()))
       .thenReturn(Future.successful(getData))
+
+    when(mockCalcConnector.fetchAndGetFormData[AcquisitionDateModel](Matchers.eq(KeystoreKeys.acquisitionDate))(Matchers.any(), Matchers.any()))
+      .thenReturn(acquisitionDateData)
+
+    when(mockCalcConnector.fetchAndGetFormData[HowBecameOwnerModel](Matchers.eq(KeystoreKeys.howBecameOwner))(Matchers.any(), Matchers.any()))
+      .thenReturn(howBecameOwnerData)
+
+    when(mockCalcConnector.fetchAndGetFormData[BoughtForLessModel](Matchers.eq(KeystoreKeys.boughtForLess))(Matchers.any(), Matchers.any()))
+      .thenReturn(boughtForLessData)
 
     new AcquisitionCostsController {
       override val calcConnector: CalculatorConnector = mockCalcConnector
@@ -55,10 +70,52 @@ class AcquisitionCostsSpec extends UnitSpec with WithFakeApplication with Mockit
     }
   }
 
-  "In CalculationController calling the .acquisitionCosts action " should {
+  "Calling the .backLink method" should {
+
+    "return a link to WorthOnLegislationStart page with an acquisition date before legislation start" in {
+      val target = setupTarget(None, acquisitionDateData = Some(AcquisitionDateModel("Yes", Some(10), Some(5), Some(1972))))
+      val result = target.getBackLink
+
+      await(result) shouldBe controllers.nonresident.routes.WorthBeforeLegislationStartController.worthBeforeLegislationStart().url
+    }
+
+    "return a link to WorthWhenGifted page with an acquisition date after legislation start and gifted option" in {
+      val target = setupTarget(None,
+        acquisitionDateData = Some(AcquisitionDateModel("Yes", Some(10), Some(5), Some(2000))),
+        howBecameOwnerData = Some(HowBecameOwnerModel("Gifted")))
+      val result = target.getBackLink
+
+      await(result) shouldBe controllers.nonresident.routes.WorthWhenGiftedToController.worthWhenGiftedTo().url
+    }
+
+    "return a link to WorthWhenInherited when property was inherited" in {
+      val target = setupTarget(None, howBecameOwnerData = Some(HowBecameOwnerModel("Inherited")))
+      val result = target.getBackLink
+
+      await(result) shouldBe controllers.nonresident.routes.WorthWhenInheritedController.worthWhenInherited().url
+    }
+
+    "return a link to BoughtForLess when property was bought for less" in {
+      val target = setupTarget(None, howBecameOwnerData = Some(HowBecameOwnerModel("Bought")), boughtForLessData = Some(BoughtForLessModel(true)))
+      val result = target.getBackLink
+
+      await(result) shouldBe controllers.nonresident.routes.WorthWhenBoughtForLessController.worthWhenBoughtForLess().url
+    }
+
+    "return a link to acquisition value when not bought for less" in {
+      val target = setupTarget(None, acquisitionDateData = Some(AcquisitionDateModel("No", None, None, None)),
+        howBecameOwnerData = Some(HowBecameOwnerModel("Bought")),
+        boughtForLessData = Some(BoughtForLessModel(false)))
+      val result = target.getBackLink
+
+      await(result) shouldBe controllers.nonresident.routes.AcquisitionValueController.acquisitionValue().url
+    }
+  }
+
+  "Calling the .acquisitionCosts action " should {
 
     "not supplied with a pre-existing stored model" should {
-      val target = setupTarget(None)
+      val target = setupTarget(None, acquisitionDateData = Some(AcquisitionDateModel("No", None, None, None)), Some(HowBecameOwnerModel("Gifted")))
       lazy val result = target.acquisitionCosts(fakeRequestWithSession)
       lazy val document = Jsoup.parse(bodyOf(result))
 
@@ -68,6 +125,10 @@ class AcquisitionCostsSpec extends UnitSpec with WithFakeApplication with Mockit
 
       "load the acquisitionCosts page" in {
         document.title shouldBe messages.question
+      }
+
+      "have a back link to the WorthWhenGiftedTo page" in {
+        document.select("#back-link").attr("href") shouldBe controllers.nonresident.routes.WorthWhenGiftedToController.worthWhenGiftedTo().url
       }
     }
 
@@ -95,7 +156,7 @@ class AcquisitionCostsSpec extends UnitSpec with WithFakeApplication with Mockit
       }
 
       "redirect to the session timeout page" in {
-        redirectLocation(result).get should include ("/calculate-your-capital-gains/session-timeout")
+        redirectLocation(result).get should include("/calculate-your-capital-gains/session-timeout")
       }
     }
   }
@@ -117,7 +178,7 @@ class AcquisitionCostsSpec extends UnitSpec with WithFakeApplication with Mockit
     }
 
     "supplied with an invalid form" should {
-      val target = setupTarget(None)
+      val target = setupTarget(None, acquisitionDateData = Some(AcquisitionDateModel("No", None, None, None)), Some(HowBecameOwnerModel("Inherited")))
       lazy val request = fakeRequestToPOSTWithSession(("acquisitionCosts", "a"))
       lazy val result = target.submitAcquisitionCosts(request)
       lazy val document = Jsoup.parse(bodyOf(result))
@@ -128,6 +189,10 @@ class AcquisitionCostsSpec extends UnitSpec with WithFakeApplication with Mockit
 
       "return to the acquisition costs page" in {
         document.title shouldBe messages.question
+      }
+
+      "have a back link to the WorthWhenInherited page" in {
+        document.select("#back-link").attr("href") shouldBe controllers.nonresident.routes.WorthWhenInheritedController.worthWhenInherited().url
       }
     }
   }
