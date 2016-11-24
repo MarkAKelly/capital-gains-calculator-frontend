63d2f8e570b6a5b5a8356b7fd07cca9c7688eb40
James Forster <james.forster@digital.hmrc.gov.uk>
2016-11-24 14:15:41 +0000

    Added new models and calculate call method.

--- test/connectors/CalculatorConnectorSpec.scala
+++ test/connectors/CalculatorConnectorSpec.scala
@@ -200,7 +200,8 @@ class CalculatorConnectorSpec extends UnitSpec with MockitoSugar {
 
   }
 
-  def setupMockedConnector(totalGainAnswersModel: TotalGainAnswersModel, totalGainResultsModel: Option[TotalGainResultsModel] = None): CalculatorConnector = {
+  def setupMockedConnector(totalGainAnswersModel: TotalGainAnswersModel, totalGainResultsModel: Option[TotalGainResultsModel] = None,
+                           calculationResultsWithPRRModel: Option[CalculationResultsWithPRRModel] = None): CalculatorConnector = {
 
     val mockSessionCache = mock[SessionCache]
     val mockHttpGet = mock[HttpGet]
@@ -232,8 +233,15 @@ class CalculatorConnectorSpec extends UnitSpec with MockitoSugar {
     when(mockSessionCache.fetchAndGetEntry[ImprovementsModel](Matchers.eq(KeystoreKeys.improvements))(Matchers.any(), Matchers.any()))
       .thenReturn(Future.successful(Some(totalGainAnswersModel.improvementsModel)))
 
-    when(mockHttpGet.GET[Option[TotalGainResultsModel]](Matchers.any())(Matchers.any(), Matchers.any()))
-      .thenReturn(totalGainResultsModel)
+    if (totalGainResultsModel.isDefined) {
+      when(mockHttpGet.GET[Option[TotalGainResultsModel]](Matchers.any())(Matchers.any(), Matchers.any()))
+        .thenReturn(totalGainResultsModel)
+    }
+
+    if (calculationResultsWithPRRModel.isDefined) {
+      when(mockHttpGet.GET[Option[CalculationResultsWithPRRModel]](Matchers.any())(Matchers.any(), Matchers.any()))
+        .thenReturn(calculationResultsWithPRRModel)
+    }
 
     new CalculatorConnector {
       override val sessionCache: SessionCache = mockSessionCache
@@ -385,6 +393,33 @@ class CalculatorConnectorSpec extends UnitSpec with MockitoSugar {
 
       await(result) shouldBe Some(validResponse)
     }
+  }
+
+  "Calling calculateTaxableGainAfterPRR" should {
+
+    val validResponse = mock[CalculationResultsWithPRRModel]
+    val model = TotalGainAnswersModel(DisposalDateModel(5, 10, 2016),
+      SoldOrGivenAwayModel(true),
+      Some(SoldForLessModel(false)),
+      DisposalValueModel(1000),
+      DisposalCostsModel(100),
+      HowBecameOwnerModel("Gifted"),
+      Some(BoughtForLessModel(false)),
+      AcquisitionValueModel(2000),
+      AcquisitionCostsModel(200),
+      AcquisitionDateModel("No", None, None, None),
+      Some(RebasedValueModel("No", None)),
+      Some(RebasedCostsModel("No", None)),
+      ImprovementsModel("Yes", Some(10), Some(20)),
+      None)
+    val target = setupMockedConnector(model, calculationResultsWithPRRModel = Some(validResponse))
+
+    "return a valid response" in {
+      val result = target.calculateTaxableGainAfterPRR(model, PrivateResidenceReliefModel("No", None, None))
+
+      await(result) shouldBe Some(validResponse)
+    }
+
   }
 
   "Calling calculateFlat" should {
