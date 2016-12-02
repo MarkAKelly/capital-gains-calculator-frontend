60fd6155d44678dc20cb16e1cc50cfca011782fe
david.woodward <david.woodward@digital.hmrc.gov.uk>
2016-12-02 12:22:06 +0000

    Added the new constructor for the rows.

--- app/constructors/nonresident/YourAnswersConstructor.scala
+++ app/constructors/nonresident/YourAnswersConstructor.scala
@@ -21,11 +21,13 @@ import models.nonresident.{PrivateResidenceReliefModel, QuestionAnswerModel, Tot
 object YourAnswersConstructor {
 
   def fetchYourAnswers(totalGainAnswersModel: TotalGainAnswersModel,
-                       privateResidenceReliefModel: Option[PrivateResidenceReliefModel] = None): Seq[QuestionAnswerModel[Any]] = {
+                       privateResidenceReliefModel: Option[PrivateResidenceReliefModel] = None,
+                       personalAndPreviousDetailsModel: Option[TotalPersonalDetailsCalculationModel] = None): Seq[QuestionAnswerModel[Any]] = {
     val salesDetailsRows = SalesDetailsConstructor.salesDetailsRows(totalGainAnswersModel)
     val purchaseDetailsRows = PurchaseDetailsConstructor.getPurchaseDetailsSection(totalGainAnswersModel)
     val propertyDetailsRows = PropertyDetailsConstructor.propertyDetailsRows(totalGainAnswersModel)
     val deductionDetailsRows = DeductionDetailsConstructor.deductionDetailsRows(totalGainAnswersModel, privateResidenceReliefModel)
+    val personalAndPreviousDetailsRows = PersonalAndPreviousDetailsConstructor.personalAndPreviousDetailsRows(personalAndPreviousDetailsModel)
 
     salesDetailsRows ++ purchaseDetailsRows ++ propertyDetailsRows ++ deductionDetailsRows
   }
