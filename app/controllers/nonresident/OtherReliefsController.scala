7e6dda0e959894aab4fd420729484b9f2e7eb4c5
Emma-Louise Upton <emma-louise.upton@hmrcaspire.com>
2016-12-09 14:51:33 +0000

    Partial commit, generalised taxable gain calculation functions for other reliefs

--- app/controllers/nonresident/OtherReliefsController.scala
+++ app/controllers/nonresident/OtherReliefsController.scala
@@ -28,6 +28,7 @@ import play.api.data.Form
 import uk.gov.hmrc.play.frontend.controller.FrontendController
 import uk.gov.hmrc.play.http.HeaderCarrier
 import views.html.calculation
+import common.nonresident.OtherReliefsTaxableGainCalculation._
 
 import scala.concurrent.Future
 
@@ -43,72 +44,6 @@ trait OtherReliefsController extends FrontendController with ValidActiveSession 
 
   override val sessionTimeoutUrl = controllers.nonresident.routes.SummaryController.restart().url
   override val homeLink = controllers.nonresident.routes.DisposalDateController.disposalDate().url
-
-  private def getPRRResponse(totalGainResultsModel: TotalGainResultsModel)(implicit hc: HeaderCarrier): Future[Option[PrivateResidenceReliefModel]] = {
-    val optionSeq = Seq(totalGainResultsModel.rebasedGain, totalGainResultsModel.timeApportionedGain).flatten
-    val finalSeq = Seq(totalGainResultsModel.flatGain) ++ optionSeq
-
-    if (finalSeq.exists(_ > 0)) {
-      calcConnector.fetchAndGetFormData[PrivateResidenceReliefModel](KeystoreKeys.privateResidenceRelief)
-    } else Future(None)
-  }
-
-  private def getPRRIfApplicable(totalGainAnswersModel: TotalGainAnswersModel,
-                         privateResidenceReliefModel: Option[PrivateResidenceReliefModel])(implicit hc: HeaderCarrier):
-    Future[Option[CalculationResultsWithPRRModel]] = {
-
-    privateResidenceReliefModel match {
-      case Some(data) => calcConnector.calculateTaxableGainAfterPRR(totalGainAnswersModel, data)
-      case None => Future.successful(None)
-    }
-  }
-
-  private def getFinalSectionsAnswers(totalGainResultsModel: TotalGainResultsModel,
-                              calculationResultsWithPRRModel: Option[CalculationResultsWithPRRModel])(implicit hc: HeaderCarrier):
-    Future[Option[TotalPersonalDetailsCalculationModel]] = {
-
-    calculationResultsWithPRRModel match {
-
-      case Some(data) =>
-        val results = data.flatResult :: List(data.rebasedResult, data.timeApportionedResult).flatten
-
-        if (results.exists(_.taxableGain > 0)) {
-          answersConstructor.getPersonalDetailsAndPreviousCapitalGainsAnswers(hc)
-        } else Future(None)
-
-      case None =>
-        val gains = totalGainResultsModel.flatGain :: List(totalGainResultsModel.rebasedGain, totalGainResultsModel.timeApportionedGain).flatten
-
-        if (gains.exists(_ > 0)) {
-          answersConstructor.getPersonalDetailsAndPreviousCapitalGainsAnswers(hc)
-        } else Future(None)
-    }
-  }
-
-  private def getMaxAEA(totalPersonalDetailsCalculationModel: Option[TotalPersonalDetailsCalculationModel],
-                taxYear: Option[TaxYearModel])(implicit hc: HeaderCarrier): Future[Option[BigDecimal]] = {
-    totalPersonalDetailsCalculationModel match {
-      case Some(data) if data.customerTypeModel.customerType.equals(CustomerTypeKeys.trustee) && data.trusteeModel.get.isVulnerable.equals("No") =>
-        calcConnector.getPartialAEA(TaxDates.taxYearStringToInteger(taxYear.get.calculationTaxYear))
-      case _ => calcConnector.getFullAEA(TaxDates.taxYearStringToInteger(taxYear.get.calculationTaxYear))
-    }
-  }
-
-  private def getTaxYear(totalGainAnswersModel: TotalGainAnswersModel)(implicit hc: HeaderCarrier): Future[Option[TaxYearModel]] = {
-    val date = totalGainAnswersModel.disposalDateModel
-    calcConnector.getTaxYear(s"${date.year}-${date.month}-${date.day}")
-  }
-
-  private def getFlatChargeableGain(totalGainAnswersModel: TotalGainAnswersModel,
-                                   prrModel: Option[PrivateResidenceReliefModel],
-                                   totalTaxOwedModel: Option[TotalPersonalDetailsCalculationModel],
-                                   maxAEA: BigDecimal)(implicit hc: HeaderCarrier): Future[Option[CalculationResultsWithTaxOwedModel]] = {
-
-    totalTaxOwedModel match {
-      case Some(data) => calcConnector.calculateNRCGTTotalTax(totalGainAnswersModel, prrModel, data, maxAEA)
-      case None => Future(None)
-    }
-  }
 
   val otherReliefs = ValidateSession.async { implicit request =>
 
@@ -128,12 +63,12 @@ trait OtherReliefsController extends FrontendController with ValidActiveSession 
     for {
       answers <- answersConstructor.getNRTotalGainAnswers(hc)
       gain <- calcConnector.calculateTotalGain(answers)(hc)
-      prrAnswers <- getPRRResponse(gain.get)
-      totalGainWithPRR <- getPRRIfApplicable(answers, prrAnswers)(hc)
-      allAnswers <- getFinalSectionsAnswers(gain.get, totalGainWithPRR)(hc)
-      taxYear <- getTaxYear(answers)(hc)
-      maxAEA <- getMaxAEA(allAnswers, taxYear)(hc)
-      chargeableGainResult <- getFlatChargeableGain(answers, prrAnswers, allAnswers, maxAEA.get)(hc)
+      prrAnswers <- getPRRResponse(gain.get, calcConnector)
+      totalGainWithPRR <- getPRRIfApplicable(answers, prrAnswers, calcConnector)(hc)
+      allAnswers <- getFinalSectionsAnswers(gain.get, totalGainWithPRR, calcConnector, answersConstructor)(hc)
+      taxYear <- getTaxYear(answers, calcConnector)(hc)
+      maxAEA <- getMaxAEA(allAnswers, taxYear, calcConnector)(hc)
+      chargeableGainResult <- getChargeableGain(answers, prrAnswers, allAnswers, maxAEA.get, calcConnector)(hc)
       reliefs <- calcConnector.fetchAndGetFormData[OtherReliefsModel](KeystoreKeys.otherReliefsFlat)
     } yield routeRequest(reliefs, gain, chargeableGainResult)
 
@@ -153,12 +88,12 @@ trait OtherReliefsController extends FrontendController with ValidActiveSession 
       for {
         answers <- answersConstructor.getNRTotalGainAnswers(hc)
         gain <- calcConnector.calculateTotalGain(answers)(hc)
-        prrAnswers <- getPRRResponse(gain.get)(hc)
-        totalGainWithPRR <- getPRRIfApplicable(answers, prrAnswers)(hc)
-        allAnswers <- getFinalSectionsAnswers(gain.get, totalGainWithPRR)(hc)
-        taxYear <- getTaxYear(answers)(hc)
-        maxAEA <- getMaxAEA(allAnswers, taxYear)(hc)
-        chargeableGainResult <- getFlatChargeableGain(answers, prrAnswers, allAnswers, maxAEA.get)(hc)
+        prrAnswers <- getPRRResponse(gain.get, calcConnector)(hc)
+        totalGainWithPRR <- getPRRIfApplicable(answers, prrAnswers, calcConnector)(hc)
+        allAnswers <- getFinalSectionsAnswers(gain.get, totalGainWithPRR, calcConnector, answersConstructor)(hc)
+        taxYear <- getTaxYear(answers, calcConnector)(hc)
+        maxAEA <- getMaxAEA(allAnswers, taxYear, calcConnector)(hc)
+        chargeableGainResult <- getChargeableGain(answers, prrAnswers, allAnswers, maxAEA.get, calcConnector)(hc)
       } yield routeRequest(gain, chargeableGainResult)
     }
 
---
+++ test/common/OtherRelievesTaxableGainCalculationSpec.scala
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package common
+
+import assets.MessageLookup.NonResident.OtherReliefs
+import connectors.CalculatorConnector
+import constructors.nonresident.AnswersConstructor
+import controllers.helpers.FakeRequestHelper
+import controllers.nonresident.OtherReliefsController
+import models.nonresident._
+import models.resident.TaxYearModel
+import org.jsoup.Jsoup
+import org.mockito.Matchers
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import uk.gov.hmrc.play.http.HeaderCarrier
+import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
+import assets.MessageLookup.NonResident.{OtherReliefs => messages}
+import scala.concurrent.ExecutionContext.Implicits.global
+import assets.MessageLookup.NonResident.{OtherReliefs => messages}
+import common.{KeystoreKeys, TestModels}
+import connectors.CalculatorConnector
+import constructors.nonresident.AnswersConstructor
+import controllers.helpers.FakeRequestHelper
+import controllers.nonresident.OtherReliefsController
+import models.nonresident._
+import models.resident.TaxYearModel
+import org.jsoup.Jsoup
+import org.mockito.Matchers
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import play.api.test.Helpers._
+import uk.gov.hmrc.play.http.HeaderCarrier
+import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
+
+import scala.concurrent.Future
+import scala.concurrent.Future
+
+class OtherRelievesTaxableGainCalculationSpec extends UnitSpec with WithFakeApplication with MockitoSugar with FakeRequestHelper{
+
+  implicit val hc = new HeaderCarrier()
+
+  def setupTarget(getData: Option[OtherReliefsModel],
+                  gainResult: CalculationResultModel,
+                  chargeableGainResult: Option[CalculationResultsWithTaxOwedModel] = None,
+                  finalSummaryModel: TotalPersonalDetailsCalculationModel,
+                  totalGainResult: Option[TotalGainResultsModel] = Some(TotalGainResultsModel(200, None, None)),
+                  calculationResultsWithPRRModel: Option[CalculationResultsWithPRRModel] = None
+                 ): OtherReliefsController = {
+
+    val mockCalcConnector = mock[CalculatorConnector]
+    val mockAnswersConstructor = mock[AnswersConstructor]
+
+    when(mockCalcConnector.fetchAndGetFormData[OtherReliefsModel](Matchers.eq(KeystoreKeys.otherReliefsFlat))(Matchers.any(), Matchers.any()))
+      .thenReturn(Future.successful(getData))
+
+    when(mockCalcConnector.fetchAndGetFormData[PrivateResidenceReliefModel](Matchers.eq(KeystoreKeys.privateResidenceRelief))(Matchers.any(), Matchers.any()))
+      .thenReturn(Future.successful(Some(PrivateResidenceReliefModel("No", None))))
+
+    when(mockCalcConnector.calculateFlat(Matchers.any())(Matchers.any()))
+      .thenReturn(Future.successful(Some(gainResult)))
+
+    when(mockAnswersConstructor.getNRTotalGainAnswers(Matchers.any()))
+      .thenReturn(Future.successful(Future.successful(TestModels.businessScenarioFiveModel)))
+
+    when(mockCalcConnector.calculateTotalGain(Matchers.any())(Matchers.any()))
+      .thenReturn(Future.successful(Future.successful(totalGainResult)))
+
+    when(mockAnswersConstructor.getPersonalDetailsAndPreviousCapitalGainsAnswers(Matchers.any()))
+      .thenReturn(Future.successful(Some(finalSummaryModel)))
+
+    when(mockCalcConnector.calculateTaxableGainAfterPRR(Matchers.any(), Matchers.any())(Matchers.any()))
+      .thenReturn(calculationResultsWithPRRModel)
+
+    when(mockCalcConnector.getFullAEA(Matchers.any())(Matchers.any()))
+      .thenReturn(Future.successful(Some(BigDecimal(11000))))
+
+    when(mockCalcConnector.getPartialAEA(Matchers.any())(Matchers.any()))
+      .thenReturn(Future.successful(Some(BigDecimal(5500))))
+
+    when(mockCalcConnector.calculateNRCGTTotalTax(Matchers.any(), Matchers.any(), Matchers.any(), Matchers.any(), Matchers.any())(Matchers.any()))
+      .thenReturn(Future.successful(chargeableGainResult))
+
+    when(mockCalcConnector.getTaxYear(Matchers.any())(Matchers.any()))
+      .thenReturn(Future.successful(Some(TaxYearModel("2015/16", true, "2015/16"))))
+
+    new OtherReliefsController {
+      override val calcConnector: CalculatorConnector = mockCalcConnector
+      override val answersConstructor: AnswersConstructor = mockAnswersConstructor
+    }
+  }
+
+  "When the otherReliefs controller is supplied with a chargeable gain of £100 and total gain of £200" should {
+
+    val finalAnswersModel = TotalPersonalDetailsCalculationModel(
+      CustomerTypeModel("individual"),
+      Some(CurrentIncomeModel(20000)),
+      Some(PersonalAllowanceModel(0)),
+      None,
+      OtherPropertiesModel("Yes"),
+      Some(PreviousLossOrGainModel("Neither")),
+      None,
+      None,
+      Some(AnnualExemptAmountModel(0)),
+      BroughtForwardLossesModel(false, None)
+    )
+
+    val chargeableGainResultModel = CalculationResultsWithTaxOwedModel(
+      TotalTaxOwedModel(100, 100, 20, None, None, 200, 100, None, None, None, None, 0, None),
+      None,
+      None
+    )
+
+    val calcResultModel = CalculationResultModel(100, 100, 100, 20, 0, None, None, None)
+
+
+    "supplied with a chargeable gain of £100 and total gain of £200" when {
+      val target = setupTarget(None,
+        calcResultModel,
+        Some(chargeableGainResultModel),
+        finalAnswersModel)
+      lazy val result = target.otherReliefs(fakeRequestWithSession)
+      lazy val document = Jsoup.parse(bodyOf(result))
+
+      "return a status of 200" in {
+        status(result) shouldBe 200
+      }
+
+      s"show help text with text '${messages.additionalHelp(200, 100)}'" in {
+        document.body().select("#otherReliefHelpTwo").select("p").text() shouldBe messages.additionalHelp(200, 100)
+      }
+    }
+  }
+}
